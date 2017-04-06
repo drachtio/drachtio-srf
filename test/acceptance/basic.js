7b727a78219b67cf6095d1ddaa1fa9b49e3e7d66
Dave Horton <daveh@beachdognet.com>
2017-04-06 10:38:30 -0400

    add test for srf locals

--- test/acceptance/basic.js
+++ test/acceptance/basic.js
@@ -53,6 +53,43 @@ describe('uac / uas scenarios', function() {
         }) ;
     }) ;    
 
+    it('should support app locals', function(done) {
+        var self = this ;
+        uac = cfg.configureUac( cfg.client[0], Agent ) ;
+        uas = require('../scripts/uas/app3')(cfg.client[1]) ;
+        cfg.connectAll([uac, uas], function(err){
+            if( err ) { throw err ; }
+
+            uac.request({
+                uri: cfg.sipServer[1],
+                method: 'INVITE',
+                body: cfg.client[0].sdp,
+                headers: {
+                    Subject: self.test.fullTitle()
+                }
+            }, function( err, req ) {
+                should.not.exist(err) ;
+                req.on('response', function(res, ack){
+                    res.should.have.property('status',200);
+                    ack() ; 
+                    
+                    setTimeout( function(){
+                        uac.request({
+                            method: 'BYE',
+                            stackDialogId: res.stackDialogId
+                        }, function(err, bye){
+                            should.not.exist(err) ;
+                            bye.on('response', function(response){
+                                response.should.have.property('status',200);
+                                uac.idle.should.be.true ;
+                                done() ;
+                            }) ;
+                        }) ;
+                    }, 1) ;
+                }) ;
+            }) ;
+        }) ;
+    }) ;    
     it('should create a UAS dialog and allow remote side to tear down', function(done) {
         var self = this ;
         uac = cfg.configureUac( cfg.client[0], Agent ) ;
---
+++ test/scripts/uas/app3.js
@@ -0,0 +1,37 @@
+var drachtio = require('drachtio') ;
+var app = drachtio() ;
+var Srf = require('../../..') ;
+var srf = new Srf(app) ;
+var fs = require('fs') ;
+var assert = require('assert'); 
+var debug = require('debug')('srf-test') ;
+var assert = require('assert');
+
+srf.locals.title = 'locals';
+
+module.exports = function( config ) {
+
+  app.set('api logger',fs.createWriteStream(config.apiLog) ) ;
+  config.connect_opts.label = config.label; 
+  srf.connect(config.connect_opts) ;
+
+  app.invite( function(req, res ) {
+    assert(srf.locals.title = 'locals');
+    srf.createUasDialog( req, res, {
+      localSdp: config.sdp
+    }, function(err, dialog) {
+      assert(!err) ;
+
+      dialog.on('destroy', function() {
+        debug('done') ;
+      }) ;
+    }) ;
+  }) ;
+
+
+  return app ;
+} ;
+
+
+
+
