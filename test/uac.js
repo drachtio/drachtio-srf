b4961be5acdbdd526fc9d964ca17edfd043c189d
Dave Horton <daveh@beachdognet.com>
2019-11-05 21:26:50 -0500

    allow headers to be attached to CANCEL request

--- test/uac.js
+++ test/uac.js
@@ -233,6 +233,36 @@ test('UAC', (t) => {
     })
 
     .then(() => {
+      srf = new Srf();
+      return connect(srf);
+    })
+    .then(() => {
+      return new Promise((resolve, reject) => {
+        let inviteSent;
+        srf.createUAC('sip:sipp-uas-cancel', {
+          headers: {
+            To: 'sip:dhorton@sip.drachtio.org',
+            From: 'sip:dhorton@sip.drachtio.org'
+          }
+        }, {
+          cbRequest: (err, req) => inviteSent = req,
+          cbProvisional: (response) => {
+            if (response.status < 200) {
+              inviteSent.cancel({headers: {'Reason': 'SIP;cause=200;text="Call completed elsewhere"'}});
+            }
+          }
+        }, (err, dlg) => {
+          if (err && err.status === 487) resolve();
+          else (reject(`expected 487 response to status, got ${err}`));
+        });
+      });
+    })
+    .then((uac) => {
+      srf.disconnect();
+      return t.pass('Srf#request can be canceled');
+    })
+
+    .then(() => {
       return t.end();
     })
     .catch((err) => {
