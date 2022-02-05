895ce9d75618b081e86b62013b6d5a417629f3c2
Joel Teixeira <joelateixeira7@gmai.com>
2022-02-05 16:06:22 +0000

    Deprecated drachtio-sip

--- test/index.js
+++ test/index.js
@@ -1,9 +1,21 @@
-require('./docker_start');
-require('./b2b');
-require('./reinvite-tests');
-require('./uac');
-require('./uas');
-require('./proxy');
-require('./utils');
-require('./refer');
-require('./docker_stop');
+const exec = require('child_process').exec;
+
+console.log('Run unit tests');
+
+exec('"npm" run unittests', (err, stdout, stderr) => {
+    console.log(stdout);
+
+    console.log('Run integration tests');
+
+    require('./docker_start');
+    require('./b2b');
+    require('./reinvite-tests');
+    require('./uac');
+    require('./uas');
+    require('./proxy');
+    require('./utils');
+    require('./refer');
+    require('./docker_stop');
+});
+
+
